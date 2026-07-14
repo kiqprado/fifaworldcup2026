8313ecfce7b4c5a32c85030fe3f7956bc4b60f93
Kaique Prado <kiqprado@outlook.com>
2026-07-14 15:45:44 -0300

    .

--- app/matches/matches-client-page.tsx
+++ app/matches/matches-client-page.tsx
@@ -386,7 +386,7 @@ export default function MatchesClientPage() {
 
       <HeaderPageTitle
         title="Partidas"
-        description="Quatro Países e último obstáculo antes de lutar pelo topo do mundo."
+        description="Quatro Países e um último obstáculo antes de lutar pelo topo do mundo."
         //"Agora é Mata-Mata raiz: quem vencer fica, quem perder volta pra casa." //
       />
 
